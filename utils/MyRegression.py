d5c0a64d4741d14526ae022c7f922b31a07b77b1
Linuxoid <ilya.glazunow@gmail.com>
2024-10-13 20:36:05 +0300

    linear regression with l2 checked

--- utils/MyRegression.py
+++ utils/MyRegression.py
@@ -8,7 +8,7 @@ class MyLinearRegression():
                  l1_ratio: float = 0,
                  l2_ratio: float = 0,
                  learning_rate: float = .1,
-                 threshold: float = .0001,
+                 threshold: float = .001,
                  early_stopping_rounds: int = 100,
                  verbose: int = 0,
                  max_iter = 100000) -> None:
@@ -26,7 +26,7 @@ class MyLinearRegression():
         X = np.insert(X, 0, 1, axis=1)
         m, n = X.shape
         self.coef_ = np.zeros(n)
-        prev_loss = float('inf')
+        min_loss = float('inf')
         no_changes_counter = 0
         
         for i in range(self.max_iter):
@@ -36,34 +36,34 @@ class MyLinearRegression():
             gradient = X.T.dot(errors) / m
             # Additional L1 or L2 Regularization
             if self.l1_ratio > 0:
-                gradient += self.l1_ratio * np.sign(self.coef_)
+                gradient[1:] += self.l1_ratio * np.sign(self.coef_[1:])
             if self.l2_ratio > 0:
-                gradient += 2 * self.l2_ratio * self.coef_
+                gradient[1:] += 2 * self.l2_ratio * self.coef_[1:]
 
             self.coef_ = self.coef_ - self.learning_rate * gradient
 
             # Loss with L1 and L2 Addition
             loss = np.sum(errors ** 2) / (2 * m)
-            if self.l2_ratio > 0:
-                loss += self.l2_ratio * np.sum(self.coef_ ** 2)
             if self.l1_ratio > 0:
-                loss += self.l1_ratio * np.sum(np.abs(self.coef_))
+                loss += self.l1_ratio * np.sum(np.abs(self.coef_[1:]))
+            if self.l2_ratio > 0:
+                loss += self.l2_ratio* np.sum(self.coef_[1:] ** 2)
             
             if self.verbose == 0:
                 pass
             elif not i % self.verbose:
                 print(f'Iteration: {i}, MSE: {loss}')
 
-            if (prev_loss - loss) <= self.threshold or (loss > prev_loss):
+            if min_loss - loss <= self.threshold:
                 no_changes_counter += 1
-                if no_changes_counter == 10:
+                if no_changes_counter == self.early_stopping_rounds:
                     self.best_iteration = i
                     return
             else:
                 no_changes_counter = 0
             
-            prev_loss = loss
-            i += 1
+            min_loss = min(min_loss, loss)
+            
         self.best_iteration = self.max_iter
 
     def predict(self, X: np.ndarray) -> np.ndarray:
