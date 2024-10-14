8bac9e08e828829edfa66d2104bf2728497f51bd
Linuxoid <ilya.glazunow@gmail.com>
2024-10-14 17:18:54 +0300

    logistic regression added

--- utils/MyRegression.py
+++ utils/MyRegression.py
@@ -1,10 +1,43 @@
 import numpy as np
 
-class MyLinearRegression():
+class MyRegression():
     """
-    Linear Regression Class
+    Regression Class for Linear and Logistic Regression with L1/L2 Regularization
+
+    This class supports both linear and logistic regression, with options for L1 (Lasso) or L2 (Ridge) regularization.
+
+    Parameters
+    ----------
+    type : str, default='linear'
+        Specifies the type of regression to perform. Options are:
+        - 'linear' for Linear Regression
+        - 'logistic' for Logistic Regression
+
+    l1_ratio : float, default=0
+        Regularization strength for Lasso (L1) regression. A value of 0 disables L1 regularization.
+        Cannot be used simultaneously with l2_ratio > 0.
+
+    l2_ratio : float, default=0
+        Regularization strength for Ridge (L2) regression. A value of 0 disables L2 regularization.
+        Cannot be used simultaneously with l1_ratio > 0.
+
+    learning_rate : float, default=0.1
+        The step size for gradient descent optimization.
+
+    threshold : float, default=0.001
+        The tolerance level for determining whether the new loss is considered better than the previous one.
+
+    early_stopping_rounds : int, default=100
+        The number of iterations to continue training without improvement before stopping early.
+
+    verbose : int, default=0
+        Controls the verbosity level. A value of 0 means no information will be printed during training.
+
+    max_iter : int, default=100000
+        The maximum number of iterations allowed for the training process.
+
     """
-    def __init__(self,
+    def __init__(self, type: str = 'linear', # 'linear' or 'logistic'
                  l1_ratio: float = 0,
                  l2_ratio: float = 0,
                  learning_rate: float = .1,
@@ -12,17 +45,48 @@ class MyLinearRegression():
                  early_stopping_rounds: int = 100,
                  verbose: int = 0,
                  max_iter = 100000) -> None:
+        if type not in ['linear', 'logistic']:
+            raise ValueError(f"Invalid regression_type '{type}'. Expected 'linear' or 'logistic'.")
+        self.type = type
         self.l1_ratio = l1_ratio
         self.l2_ratio = l2_ratio
+        if (l1_ratio > 0) and (l2_ratio > 0):
+            raise ValueError(f"Couldn't use L1 and L2 simultaneuosly.")
         self.learning_rate = learning_rate
         self.threshold = threshold
         self.early_stopping_rounds = early_stopping_rounds
         self.verbose = verbose
         self.max_iter = max_iter
         self.coef_ = None
-        self.best_iteration = None
+        self.best_iteration_ = None
+
+    def _sigmoid(self, z: np.ndarray) -> np.ndarray:
+        """
+        Private Function to compute Sigmoid in .fit() method 
+        """
+        return 1 / (1 + np.exp(-z))
+    
+    def _loss_computation(self, predictions: np.ndarray, y: np.ndarray, m: int) -> float:
+        """
+        Private Function to compute loss in .fit() method 
+        """  
+        if self.type == 'linear':
+            errors = predictions - y
+            loss = np.sum(errors ** 2) / (2 * m)
+        elif self.regression_type == 'logistic':
+            loss = -np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions)) / m
+
+        if self.l1_ratio > 0:
+                loss += self.l1_ratio * np.sum(np.abs(self.coef_[1:]))
+        if self.l2_ratio > 0:
+            loss += self.l2_ratio* np.sum(self.coef_[1:] ** 2)
+
+        return loss
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Main .fit() method
+        """
         X = np.insert(X, 0, 1, axis=1)
         m, n = X.shape
         self.coef_ = np.zeros(n)
@@ -31,8 +95,11 @@ class MyLinearRegression():
         
         for i in range(self.max_iter):
             predictions = X.dot(self.coef_)
+            if self.type == 'logistic':
+                predictions = self._sigmoid.dot(predictions)
             errors = predictions - y
 
+            # Counting Gradient
             gradient = X.T.dot(errors) / m
             # Additional L1 or L2 Regularization
             if self.l1_ratio > 0:
@@ -42,12 +109,8 @@ class MyLinearRegression():
 
             self.coef_ = self.coef_ - self.learning_rate * gradient
 
-            # Loss with L1 and L2 Addition
-            loss = np.sum(errors ** 2) / (2 * m)
-            if self.l1_ratio > 0:
-                loss += self.l1_ratio * np.sum(np.abs(self.coef_[1:]))
-            if self.l2_ratio > 0:
-                loss += self.l2_ratio* np.sum(self.coef_[1:] ** 2)
+            # Loss Computation
+            loss = self._loss_computation(predictions, y, m)
             
             if self.verbose == 0:
                 pass
@@ -57,15 +120,18 @@ class MyLinearRegression():
             if min_loss - loss <= self.threshold:
                 no_changes_counter += 1
                 if no_changes_counter == self.early_stopping_rounds:
-                    self.best_iteration = i
+                    self.best_iteration_ = i
                     return
             else:
                 no_changes_counter = 0
             
             min_loss = min(min_loss, loss)
             
-        self.best_iteration = self.max_iter
+        self.best_iteration_ = self.max_iter
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Main .predict() Method
+        """
         X = np.insert(X, 0, 1, axis=1)
         return X.dot(self.coef_)
